07d621ca1f53f5c4eea96a7b60a5e9140dc1698c
Evgeny Agronsky <evgeny.agronsky@gmail.com>
2016-12-18 22:19:50 +0200

    Final indeed

--- src/AST/AST_ExpNewTypeArray.java
+++ src/AST/AST_ExpNewTypeArray.java
@@ -3,11 +3,11 @@ package AST;
 public class AST_ExpNewTypeArray extends AST_Exp
 {
 	public AST_Type arrayType;
-	public AST_Exp arraySize;
+	public AST_Exp	sizeExpression;
 
 	public AST_ExpNewTypeArray(AST_Type type, AST_Exp exp)
 	{
 		this.arrayType = type;
-		this.arraySize = exp;
+		this.sizeExpression = exp;
 	}
 }
--- src/AST/AST_TypeArray.java
+++ src/AST/AST_TypeArray.java
@@ -2,7 +2,6 @@ package AST;
 
 public class AST_TypeArray extends AST_Type
 {
-
 	public AST_Type arrayType;
 	
 	public AST_TypeArray(AST_Type type)
