288d8dd11b2ca9d3acf6e7e84663499a3b4d772c
Yehonatan Sayag <ysayag@Yehonatans-MacBook-Pro.local>
2016-12-23 16:43:19 +0200

    SemanticEvaluator and stuff

--- src/ic/compiler/SemanticEvaluator.java
+++ src/ic/compiler/SemanticEvaluator.java
@@ -1,7 +1,7 @@
 package ic.compiler;
-import ic.ast.AST_Node;
+import ic.ast.*;
 
-public class SemanticEvaluator {
+public class SemanticEvaluator implements Visitor<SymbolTable, Attribute> {
 	
 	//all the tests will be here
 	// (for example: check if types are matching in binary operations, check if a variable is declared..etc)
@@ -17,11 +17,16 @@ public class SemanticEvaluator {
 	public void evaluate() {
 	
 		this.program= new SymbolTable(null, "");
+		root.accept(this, program);
 		//need to call "accept" function.
 		// accept is a method of every AST_Node that calls Visitor.visit . (TODO)
 	}
-	
-	
+
+	public ResultType visit(AST_Exp expr, ContextType d) {
+		if (expr instanceof AST_Exp )
+	}
+
+
 	//TODO:   "VISIT" methods, with the tests .
 	
 }
