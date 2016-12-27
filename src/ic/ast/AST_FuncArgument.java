7b60f083da4a24b94c3bd65f97a5b429c300f40f
Yehonatan Sayag <sayag11@gmail.com>
2016-12-27 15:27:33 +0200

    equals and hasCode added to AST_FuncArgument

--- src/ic/ast/AST_FuncArgument.java
+++ src/ic/ast/AST_FuncArgument.java
@@ -5,6 +5,37 @@ import ic.compiler.Visitor;
 
 public class AST_FuncArgument extends AST_Node
 {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((argName == null) ? 0 : argName.hashCode());
+		result = prime * result + ((argType == null) ? 0 : argType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AST_FuncArgument other = (AST_FuncArgument) obj;
+		if (argName == null) {
+			if (other.argName != null)
+				return false;
+		} else if (!argName.equals(other.argName))
+			return false;
+		if (argType == null) {
+			if (other.argType != null)
+				return false;
+		} else if (!argType.equals(other.argType))
+			return false;
+		return true;
+	}
+
 	public String argName;
 	public AST_Type argType;
 
@@ -36,12 +67,6 @@ public class AST_FuncArgument extends AST_Node
 	
 	public String getArgName() {
 		return argName;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		AST_FuncArgument other = (AST_FuncArgument) obj;
-		return this.argName.equals(other.argName) && this.argType.equals(other.argType);
 	}
 
 }
